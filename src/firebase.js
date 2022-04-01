4eb4feaf3cb9e2d4d5ab6ea6e781c02b1f42fe20
NaYuJin <00skdbwls@gmail.com>
2022-04-01 13:00:55 +0900

    1.0 React + Firebase Setup
    create react app and firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyAUqIfM9T0rxFBeGOrIcTmxLmfsAVWNOGs",
+    authDomain: "nwitter2-c5741.firebaseapp.com",
+    projectId: "nwitter2-c5741",
+    storageBucket: "nwitter2-c5741.appspot.com",
+    messagingSenderId: "660160312384",
+    appId: "1:660160312384:web:0f750c85fa4125bc3cdba5"
+  };
+  
+export default firebase.initializeApp(firebaseConfig);
