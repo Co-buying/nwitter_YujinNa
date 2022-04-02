845237c9bea0756be0c9d2acd9cba2dd47f1c092
NaYuJin <00skdbwls@gmail.com>
2022-04-03 03:02:32 +0900

    3.2 Getting the Nweets
    QuerySnapshot

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home =()=> { 
     const [nweet,setNweet]=useState("");
+    const [nweets,setNweets]=useState([]);
+    const getNweets= async()=>{
+        const dbNweets=await dbService.collection("nweets").get();
+        dbNweets.forEach((document)=>{
+            const nweetObject={
+                ...document.data(),
+                id: document.id, //document.id값을 가져옴
+            }
+            setNweets(prev => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(()=>{
+        getNweets();
+    },[]);
     const onSubmit= async(event)=>{
         event.preventDefault();
         await dbService.collection("nweets").add({
@@ -27,6 +41,11 @@ const Home =()=> {
                 />
                 <input type="submit" value="Nweet" />
             </form>
+            <div key={nweet.id}>
+                {nweets.map(nweet=><div>
+                   <h4>{nweet.nweet}</h4> 
+                </div>)}
+            </div>
         </div>
     );
 }
