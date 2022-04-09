5a72a32a753d38ce03673c9c236bf3d01c7bfd84
NaYuJin <00skdbwls@gmail.com>
2022-04-09 22:18:36 +0900

    3.4 Error...
    editing error

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,19 @@ import { authService } from "fbase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 function App() {
   const [init,setInit]=useState(false);
-  const [isLoggedIn,setIsLoggenIn]=useState(false);
   const [userObj,setUserObj]=useState(null);
   useEffect(()=>{
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
     if (user) {
-        setIsLoggenIn(true);
         setUserObj(user);
-      }
-      else{
-        setIsLoggenIn(false);
       }
       setInit(true);
     });
   },[]);
   return (
     <>
-      {init? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "Initializing.."}
+      {init? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "Initializing.."}
       <footer>&copy;Nwitter{new Date().getFullYear(
 
       )}</footer>
--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,15 +1,24 @@
 import React from "react";
 
-const Nweet=({nweetObj, isOwner})=>(
-    <div>
-        <h4>{nweetObj.text}</h4>
-        {isOwner && (
-            <>
-                <button>Delete Nweet</button>
-                <button>Edit Nweet</button>
-            </>
-        )}
-    </div>
-);
+const Nweet=({nweetObj, isOwner})=>{
+    const onDeleteClick=()=>{
+        const ok=window.confirm("Are you sure you want to delete this nweet?");
+        
+        if(ok){
+            //delete
+        }
+    }
+    return (
+        <div>
+            <h4>{nweetObj.text}</h4>
+            {isOwner && (
+                <>
+                    <button onClick={onDeleteClick}>Delete Nweet</button>
+                    <button>Edit Nweet</button>
+                </>
+            )}
+        </div>
+    );
+};
 
 export default Nweet;
